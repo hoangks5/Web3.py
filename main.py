6e41eccadf44485b4ec1f50f849dc96ab047990c
hoangks5 <hoangnd.ute@gmail.com>
2022-08-15 14:24:33 +0700

    --- Web3 hoangks5 ---

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import time, json
 bsc = "https://bsc-dataseed.binance.org/"
 web3 = Web3(Web3.HTTPProvider(bsc))
 print(web3.isConnected())
+
+
+
+address = ‘ You Address Wallet ’
+balance = web3.eth.get_balance(address)
+print(balance)
+ 
+result = web3.fromWei(balance,’ether’)
+print(result)
