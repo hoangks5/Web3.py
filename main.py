cda22cb0f72edcd921f956aefb12993e87eceef5
hoangks5 <hoangnd.ute@gmail.com>
2022-08-15 14:36:13 +0700

    --- Web3 hoangks5 ---

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from web3 import Web3
 import time, json
-bsc = "https://bsc-dataseed.binance.org/"
+#bsc = "https://bsc-dataseed.binance.org/"
+bsc = 'https://data-seed-prebsc-1-s1.binance.org:8545/'
 web3 = Web3(Web3.HTTPProvider(bsc))
 print(web3.isConnected())
 
